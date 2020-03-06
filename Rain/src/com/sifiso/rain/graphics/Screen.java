1e6866866a14fd69c34bf46b4abaee0462ceac36
Sifiso Patel <sfsptl42@gmail.com>
2020-03-06 10:43:00 +0200

    Created a clear method to clear the screen and a render method to render pixels in the Screen.java class

--- Rain/src/com/sifiso/rain/graphics/Screen.java
+++ Rain/src/com/sifiso/rain/graphics/Screen.java
@@ -9,6 +9,23 @@ public class Screen {
 		
 		this.width = width;
 		this.height = height;
+		pixels = new int [width * height];
 	}
+	
+	public void clear(){
+		
+		for (int i = 0; i < pixels.length; i++){
+			
+			pixels[i] = 0;
+		}
+	}
+	
+	public void render(){
+		
+		for (int y = 0; y < height; y++){
+			for (int x = 0; x < width; x++){
+				pixels[x + y * width] = 0xff00ff;
+			}
+		}
 
 }
