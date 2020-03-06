2f0ebf7370d01b7f8a5c7ab354c43b64360a80c8
Sifiso Patel <sfsptl42@gmail.com>
2020-03-06 10:30:01 +0200

    "Created a method to clear the screen and to render pixels"

--- Rain/src/com/sifiso/rain/Game.java
+++ Rain/src/com/sifiso/rain/Game.java
@@ -16,8 +16,7 @@ public class Game extends Canvas implements Runnable{
 
 	public static int width = 300;
 	public static int height = width / 16 * 9;
-	public static int scale = 3;
-	
+	public static int scale = 3; 
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
