b06b4d93d7bcea3d03e5f27051f1f9a6146e40cc
Sifiso Patel <sfsptl42@gmail.com>
2020-10-22 16:07:18 +0200

    Implemented frame counters, cleared screen, dealt with out of bounds and negative bounds, as well as rendering pixels...FPS is low in the 160's when i want it in the 2000's

--- Rain/src/com/sifiso/rain/Game.java
+++ Rain/src/com/sifiso/rain/Game.java
@@ -7,75 +7,108 @@ import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
+import com.sifiso.rain.graphics.Screen;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
-	
+public class Game extends Canvas implements Runnable {
+
 	private static final long serialVersionUID = 1L;
 
 	public static int width = 300;
 	public static int height = width / 16 * 9;
-	public static int scale = 3; 
+	public static int scale = 3;
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
-	
-	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); //Created an image
-	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();//Accessing the image
-	
-	public Game(){
+	private String title = "Rain";
+	private Screen screen;
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); // Created an image
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();// Accessing the image
+
+	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
-		
+
+		screen = new Screen(width, height);
+
 		frame = new JFrame();
 	}
-	
-	public synchronized void start(){
+
+	public synchronized void start() {
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
-	
-	public synchronized void stop(){
+
+	public synchronized void stop() {
 		running = false;
-		
-		try{
+
+		try {
 			thread.join();
-		}catch( InterruptedException e){
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public void run(){
-		while(running){
-			update();
+
+	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
+		while (running) {
+
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
+
 			render();
+			frames++;
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				frame.setTitle(title + "  |  " + updates + " ups, " + frames + " fps");
+				updates = 0;
+				frames = 0;
+			}
 		}
+		stop();
 	}
-	
-	public void update(){
-		
+
+	public void update() {
+
 	}
-	
-	public void render(){
+
+	public void render() {
 		BufferStrategy bs = getBufferStrategy();
-		if(bs == null){
+		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
-		
+		screen.clear();
+		screen.render();
+
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+
 		Graphics g = bs.getDrawGraphics();
-		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
-	
-	
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
 		game.frame.setTitle("Rain");
@@ -84,8 +117,8 @@ public class Game extends Canvas implements Runnable{
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-		
+
 		game.start();
 	}
-	
+
 }
--- Rain/src/com/sifiso/rain/graphics/Screen.java
+++ Rain/src/com/sifiso/rain/graphics/Screen.java
@@ -1,31 +1,46 @@
 package com.sifiso.rain.graphics;
 
 public class Screen {
-	
+
 	private int width, height;
-	public int [] pixels;
-	
-	public Screen(int width, int height){
-		
+	public int[] pixels;
+
+	int xtime = 0;
+	int ytime = 0;
+	int counter = 0;
+
+	public Screen(int width, int height) {
+
 		this.width = width;
 		this.height = height;
-		pixels = new int [width * height];
+		pixels = new int[width * height];
 	}
-	
-	public void clear(){
-		
-		for (int i = 0; i < pixels.length; i++){
-			
+
+	public void clear() {
+
+		for (int i = 0; i < pixels.length; i++) {
+
 			pixels[i] = 0;
 		}
 	}
-	
-	public void render(){
-		
-		for (int y = 0; y < height; y++){
-			for (int x = 0; x < width; x++){
-				pixels[x + y * width] = 0xff00ff;
+
+	public void render() {
+		counter++;
+		if (counter % 10 == 0)
+			xtime++;
+		if (counter % 80 == 0)
+			ytime++;
+
+		for (int y = 0; y < height; y++) {
+			if (ytime < 0 || ytime >= height)
+				break;
+			for (int x = 0; x < width; x++) {
+				if (xtime < 0 || xtime >= width)
+					break;
+				pixels[xtime + ytime * width] = 0xff00ff;
 			}
 		}
+
+	}
 
 }
